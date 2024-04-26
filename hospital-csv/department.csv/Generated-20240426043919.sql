b15a610a5d8d5fc8d7c8ac5e3b14cb9c9ffb7716
Abhi... <8083613+AbhimanyuAryan@users.noreply.github.com>
2024-04-26 05:12:22 +0100

    csv

---
+++ hospital-csv/department.csv/Generated-20240426043919.sql
@@ -0,0 +1,7 @@
+--------------------------------------------------------
+--  File created - Friday-April-26-2024   
+--------------------------------------------------------
+@/Users/abhi/Desktop/hospital/department.csv/DEPARTMENT.sql
+--@/Users/abhi/Desktop/hospital/department.csv/DEPARTMENT_DATA_TABLE.csv
+@/Users/abhi/Desktop/hospital/department.csv/PK_DEPARTMENT.sql
+@/Users/abhi/Desktop/hospital/department.csv/DEPARTMENT_CONSTRAINT.sql
